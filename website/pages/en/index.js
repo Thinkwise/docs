705341ab5b3912b35b3c7df3b9904c5abe53af78
Jasper Kloost <jkloost@thinkwisesoftware.com>
2019-11-05 08:57:49 +0100

    Changed order of tiles

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -41,36 +41,36 @@ class Index extends React.Component {
               align="center"
               contents={[
                 {
+                  title: `[Overview](${siteConfig.baseUrl}docs/platform_overview.html)`,
                   content: 'A high level overview of the Thinkwise Platform.',
                   image: `${siteConfig.baseUrl}img/Indicium.jpg`,
                   imageAlign: 'top',
                   imageLink: `${siteConfig.baseUrl}docs/platform_overview.html`,
                   imageAlt: 'Overview',
-                  title: `[Overview](${siteConfig.baseUrl}docs/platform_overview.html)`,
                 },
                 {
+                  title: `[Thinkwise Software Factory](${siteConfig.baseUrl}docs/sf/sf_general.html)`,
                   content: `Learn how to create business applications with the Thinkwise Software Factory development environment.`,
                   image: `${siteConfig.baseUrl}img/SF.jpg`,
                   imageAlign: 'top',
                   imageLink: `${siteConfig.baseUrl}docs/sf/sf_general.html`,
                   imageAlt: 'Thinkwise Software Factory',
-                  title: `[Thinkwise Software Factory](${siteConfig.baseUrl}docs/sf/sf_general.html)`,
                 },
                 {
+                  title: `[Intelligent Application Manager](${siteConfig.baseUrl}docs/iam/iam_general.html)`,
                   content: `Manage your applications, users and preferences with the Thinkwise Intelligent Application Manager.`,
                   image: `${siteConfig.baseUrl}img/IAM.jpg`,
                   imageAlign: 'top',
                   imageLink: `${siteConfig.baseUrl}docs/iam/iam_general.html`,
                   imageAlt: 'Intelligent Application Manager',
-                  title: `[Intelligent Application Manager](${siteConfig.baseUrl}docs/iam/iam_general.html)`,
                 },
                 {
+                  title: `[Indicium Application Tier](${siteConfig.baseUrl}docs/indicium/indicium_general.html)`,
                   content: 'Work with the OData API of the Thinkwise Indicium Application Tier.',
                   image: `${siteConfig.baseUrl}img/Indicium.jpg`,
                   imageAlign: 'top',
                   imageLink: `${siteConfig.baseUrl}docs/indicium/indicium_general.html`,
                   imageAlt: 'Indicium Application Tier',
-                  title: `[Indicium Application Tier](${siteConfig.baseUrl}docs/indicium/indicium_general.html)`,
                 },
               ]}
               layout="fourColumn"
@@ -80,83 +80,82 @@ class Index extends React.Component {
               align="center"
               contents={[
                 {
-                  content: `Deploying the Thinkwise Platform and applications.`,
-                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
-                  imageAlign: 'top',
-                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
-                  imageAlt: 'Deployment',
-                  title: `[Deployment](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
-                },
-                {
-                  content: `In-depth information on working with the Thinkwise Platform.`,
-                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
-                  imageAlign: 'top',
-                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
-                  imageAlt: 'Knowledge Base',
-                  title: `[Knowledge Base](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
-                },                
-                {
-                  content: `Learn about Thinkwise Platform releases and updates.`,
-                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
-                  imageAlign: 'top',
-                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
-                  imageAlt: 'Releases',
-                  title: `[Releases](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
-                },
-                {
+                  title: `[Upcycler](https://thinkwisesoftware.docebosaas.com/learn)`,
                   content: `The Thinkwise Platform Upcycler.`,
                   image: `${siteConfig.baseUrl}img/Learn.jpg`,
                   imageAlign: 'top',
                   imageLink: 'https://thinkwisesoftware.docebosaas.com/learn',
                   imageAlt: 'Upcycler',
-                  title: `[Upcycler](https://thinkwisesoftware.docebosaas.com/learn)`,
                 },
-              ]}
-              layout="fourColumn"
-            />
-<br />
-            <GridBlock
-              align="center"
-              contents={[
                 {
+                  title: `[Deployment](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
+                  content: `Deploying the Thinkwise Platform and applications.`,
+                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
+                  imageAlign: 'top',
+                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
+                  imageAlt: 'Deployment',
+                },        
+                {
+                  title: `[Releases](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
+                  content: `Learn about Thinkwise Platform releases and updates.`,
+                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
+                  imageAlign: 'top',
+                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
+                  imageAlt: 'Releases',
+                },
+                {
+                  title: `[Knowledge Base](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
                   content: `In-depth information on working with the Thinkwise Platform.`,
                   image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
                   imageAlign: 'top',
                   imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
                   imageAlt: 'Knowledge Base',
+                },        
+              ]}
+              layout="fourColumn"
+            />
+            {/* <GridBlock
+              align="center"
+              contents={[
+                {
                   title: `[Knowledge Base](${siteConfig.baseUrl}docs/kb/kb_general.html)`,
+                  content: `In-depth information on working with the Thinkwise Platform.`,
+                  image: `${siteConfig.baseUrl}img/Knowledge Base.jpg`,
+                  imageAlign: 'top',
+                  imageLink: `${siteConfig.baseUrl}docs/kb/kb_general.html`,
+                  imageAlt: 'Knowledge Base',
                 },
                 {
+                  title: `[Learn](https://thinkwisesoftware.docebosaas.com/learn)`,
                   content: `Become even better with Thinkwise. Follow online courses to learn all about the Thinkwise Platform.`,
                   image: `${siteConfig.baseUrl}img/Learn.jpg`,
                   imageAlign: 'top',
                   imageLink: 'https://thinkwisesoftware.docebosaas.com/learn',
                   imageAlt: 'Learn',
-                  title: `[Learn](https://thinkwisesoftware.docebosaas.com/learn)`,
                 },
                 {
+                  title: `[Community](https://community.thinkwisesoftware.com/)`,
                   content: `Ask a question, submit your idea or get inspired by our blogs in the Thinkwise Community.`, //Need more help? Join our community!`,
                   image: `${siteConfig.baseUrl}img/Community.jpg`,
                   imageAlign: 'top',
                   imageLink: 'https://community.thinkwisesoftware.com',
                   imageAlt: 'Community',
-                  title: `[Community](https://community.thinkwisesoftware.com/)`,
                 },
               ]}
               layout="fourColumn"
-            />        
+            />         */}
           </Container>  
             <Container background="light" padding={['top', 'bottom']}>
               <GridBlock
                 align="left"
                 contents={[
                   {
+                    title: `[Community](https://community.thinkwisesoftware.com/)`,
                     content: `Ask a question, submit your idea or get inspired by our blogs in the Thinkwise Community.`, 
                     image: `${siteConfig.baseUrl}img/Community.jpg`,
                     imageAlign: 'right',
                     imageLink: 'https://community.thinkwisesoftware.com',
                     imageAlt: 'Community',
-                    title: `[Community](https://community.thinkwisesoftware.com/)`,
                   },
                 ]}
                 layout="twoColumn"
@@ -167,12 +166,12 @@ class Index extends React.Component {
                 align="left"
                 contents={[
                   {
+                    title: `[Learn](https://thinkwisesoftware.docebosaas.com/learn)`,
                     content: `Become even better with Thinkwise. Follow online courses to learn all about the Thinkwise Platform.`,
                     image: `${siteConfig.baseUrl}img/Learn.jpg`,
                     imageAlign: 'left',
                     imageLink: 'https://thinkwisesoftware.docebosaas.com/learn',
                     imageAlt: 'Learn',
-                    title: `[Learn](https://thinkwisesoftware.docebosaas.com/learn)`,
                   },
                 ]}
                 layout="twoColumn"
